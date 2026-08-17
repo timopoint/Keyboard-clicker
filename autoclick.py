d06f95782ad1b51a42b3c7fde1740cd91b466bf0
timopoint <timgolike@gmail.com>
2026-08-18 01:02:34 +0300

    Tkinter used
    new version of programm:
    added UI with tkinter
    using one json file neither three txt

--- autoclick.py
+++ autoclick.py
@@ -1,65 +1,123 @@
+import tkinter as tk
 import keyboard
-import mouse
 import time
-import os
-
-f = open("Hotkey.txt", 'a+', encoding='utf-8')
-f.seek(0)
-hotkey = f.read()
-
-if hotkey == "":
-    hotkey = 'f1'
-    f.seek(0)
-    f.write(hotkey)
-
-f.close()
+import json
+import threading as thr
 
 
+try:
+    with open("Options.json", "r", encoding="utf-8") as file:
+        pass
+except:
+    with open("Options.json", "w", encoding="utf-8") as file:
+        json.dump( {"hotkey" : "f1", "text" : "", "delay" : 0.5}, file, indent=4, ensure_ascii=False, sort_keys=True)
 
+data = {
+    "hotkey" : "",
+    "text" : "",
+    "delay" : 0.5
+}
 
-f = open("AbortHotkey.txt", 'a+', encoding='utf-8')
-f.seek(0)
-aborthotkey = f.read()
-
-if aborthotkey == "":
-    aborthotkey = 'f2'
-    f.seek(0)
-    f.write(aborthotkey)
-    
-f.close()
-
-
+with open("Options.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
 
 
 work = False
-def change():
+
+def typing_loop():
+    while work:
+        keyboard.write(data["text"])
+        time.sleep(data["delay"])
+
+def clicker():
     global work
-    work = not work
-    time.sleep(0.5)
-    print(f" '{hotkey}' - pressed ")
-
-
-def out():
-    os.abort()
-
-keyboard.add_hotkey(hotkey, change)
-
-keyboard.add_hotkey(aborthotkey, out )
-
-f = open('./Keyboard.txt', 'a+', encoding='utf-8')
-f.seek(0)
-content = f.read()
-f.close()
-print (f" text: {content}")
-
-
-print("script is working")
-print(f"to turn it on/off press '{hotkey}' ")
-print(f"to abort clicker press '{aborthotkey}' ")
-
-while True:
-    if work: 
-        keyboard.write(f"{content}")
-        time.sleep(0.2) 
+    if not work:
+        work = True
+        thr.Thread(target=typing_loop, daemon=True).start()
     else:
-        time.sleep(0.05) 
+        work = False
+
+def change_hotkey():
+    keyboard.remove_hotkey(clicker)
+    global data
+    event = keyboard.read_event()
+    data["hotkey"] = event.name       
+    hotkey_label.config(text = "hotkey: " + data["hotkey"])
+    man = keyboard.add_hotkey(data["hotkey"], clicker)
+
+def set_text():
+    global data
+    data["text"] = text_entry.get()
+    text_label.config(text= "saved text: " + data["text"])
+
+def set_delay():
+    global data
+    data["delay"] = float(time_entry.get())
+    time_label.config(text= "seconds: " + str(data["delay"]))
+
+def finish():
+    with open("Options.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False, sort_keys=True)
+    root.destroy()
+
+keyboard.add_hotkey( data["hotkey"], clicker)
+
+
+
+
+#
+root = tk.Tk()
+
+root.title("Keyboard Clicker")
+
+root.geometry("500x130")
+root.resizable(False, False)
+#
+
+
+
+#
+hotkey_label = tk.Label(root, text = "hotkey: " + data["hotkey"])
+hotkey_label.place(x= 110, y= 12 )
+
+change_hotkey_btn = tk.Button(root, text= "change hotkey", command= change_hotkey )
+change_hotkey_btn.place(x= 12, y = 10, width= 90)
+#
+
+
+
+#
+set_text_btn = tk.Button(root, text= "set text", command= set_text )
+set_text_btn.place(x= 12, y = 40, width= 90)
+
+text_entry = tk.Entry(root)
+text_entry.config(width=51)
+text_entry.place(x = 175, y= 44)
+
+text_hint_label = tk.Label(root, text = "Enter text: ")
+text_hint_label.place(x = 110, y= 42)
+
+text_label = tk.Label(root, text = "saved text: " + data["text"])
+text_label.place(x= 110, y= 65 )
+#
+
+
+
+#
+time_entry =tk.Entry(root)
+time_entry.place(x = 210, y= 90)
+
+time_hint_label = tk.Label(root, text= "Delay in seconds:")
+time_hint_label.place(x = 110, y= 90)
+
+time_label = tk.Label(root, text= "seconds: " + str(data["delay"]))
+time_label.place(x = 340, y= 90)
+
+set_time_btn = tk.Button(root, text= "set delay", command= set_delay)
+set_time_btn.place(x= 12, y = 88, width= 90)
+#
+
+
+root.protocol("WM_DELETE_WINDOW", finish)
+
+root.mainloop()
