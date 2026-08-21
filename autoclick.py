b73e23533d87a4cb6774a7972011525efd6afcf3
timopoint <timgolike@gmail.com>
2026-08-21 14:00:16 +0300

    First look at interpretation code for your macro

--- autoclick.py
+++ autoclick.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 import keyboard
 import mouse
 import time
@@ -9,7 +10,7 @@ import threading as thr
 
 
 
-#
+# Root init
 root = tk.Tk()
 
 root.title("Keyboard Clicker")
@@ -18,22 +19,27 @@ root.geometry("500x300")
 root.resizable(False, False)
 #
 
+# Preloading, variables and defines
 
+data = {
+    "hotkey" : "f1",
+    "text" : "",
+    "delay" : 0.5,
+    "text_on" : 0,
+    "mouse_on" : 0,
+    "double_on" : 0,
+    "code_on" : 0,
+    "code_path" : "",
+}
 
-
-#
 try:
     with open("Options.json", "r", encoding="utf-8") as file:
         pass
 except:
     with open("Options.json", "w", encoding="utf-8") as file:
-        json.dump( {"hotkey" : "f1", "text" : "", "delay" : 0.5}, file, indent=4, ensure_ascii=False, sort_keys=True)
+        json.dump( data, file, indent=4, ensure_ascii=False, sort_keys=True)
 
-data = {
-    "hotkey" : "",
-    "text" : "",
-    "delay" : 0.5
-}
+
 
 with open("Options.json", "r", encoding="utf-8") as file:
     data = json.load(file)
@@ -41,11 +47,13 @@ with open("Options.json", "r", encoding="utf-8") as file:
 
 work = False
 
-text_on = tk.IntVar(value= 0)
+lines = []
 
-mouse_on = tk.IntVar(value= 0)
+text_on = tk.IntVar(value= data["text_on"])
+mouse_on = tk.IntVar(value= data["mouse_on"])
+double_on = tk.IntVar(value= data["double_on"])
+code_on = tk.IntVar(value= data["code_on"])
 
-double_on = tk.IntVar(value= 0)
 
 def typing_loop():
     while work:
@@ -55,7 +63,7 @@ def typing_loop():
 def clicking_loop():
     global work
     selected = mouse_buttons[mouse_buttons_list.curselection()[0]]
-    if double_on.get() == 1:
+    if data["double_on"].get() == 1:
         while work:
             mouse.double_click(button= selected)
             time.sleep(data["delay"])
@@ -65,6 +73,8 @@ def clicking_loop():
             time.sleep(data["delay"])
             
 
+
+
 def clicker():
     global work
     if not work:
@@ -73,6 +83,8 @@ def clicker():
             thr.Thread(target=typing_loop, daemon=True).start()
         if mouse_on.get() == 1:
             thr.Thread(target=clicking_loop, daemon=True).start()
+        if code_on.get() == 1:
+            thr.Thread().start()
     else:
         work = False
 
@@ -95,16 +107,91 @@ def set_delay():
     delay_label.config(text= "Delay: " + str(data["delay"]))
 
 def finish():
+    global data
+    data["text_on"] = text_on.get()
+    data["mouse_on"] = mouse_on.get()
+    data["double_on"] = double_on.get()
+    data["code_on"] = code_on.get()
+
     with open("Options.json", "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False, sort_keys=True)
     root.destroy()
+
+
+def select_file():
+    # Открытие диалога выбора файла
+    file_path = filedialog.askopenfilename(defaultextension="txt")
+    if file_path:
+        data["code_path"] = file_path
+        code_path_label.config (text= "Path: " + data["code_path"] )
+
+
+intetpretation = {
+    "INFLOOP" : 0,
+    "FOR" : 0,
+    "ENDFOR" : 0,
+    "KEY_PRESS" : 0,
+    "KEY_DOWN" : 0,
+    "KEY_UP" : 0,
+    "MOUSE_DOUBLE_CLICK" :0,
+    "MOUSE_CLICK" :0,
+    "MOUSE_DOWN" : 0,
+    "MOUSE_UP" : 0,
+    "WAIT" : 0,
+}
+
+def INFLOOP(Lines: list[str]):
+    pass
+
+def FOR(count: int, Lines: list[str]):
+    pass
+
+
+def KEY_PRESS(key: str):
+    pass
+
+def KEY_DOWN(key: str):
+    pass
+def KEY_DOWN(key: str, time: float):
+    pass
+
+def KEY_UP(key: str):
+    pass
+
+
+def MOUSE_DOUBLE_CLICK(key: str):
+    pass
+
+def MOUSE_CLICK(key: str):
+    pass
+
+def MOUSE_DOWN(key: str):
+    pass
+def MOUSE_DOWN(key: str, time: float):
+    pass
+
+def MOUSE_UP(key: str):
+    pass
+
+
+def WAIT(seconds: float):
+    time.sleep(seconds)
+
+
+def interpretator():
+    try:
+        with open(data["code_path"], "read", encoding="utf-8") as file:
+            global lines
+            lines = file.readlines() 
+    except:
+        pass
 
 keyboard.add_hotkey( data["hotkey"], clicker)
 #
 
 
 
-#
+# Base Frame for Every option
 base_frame = tk.Frame(borderwidth=1, relief="solid")
 
 ##
@@ -134,15 +221,26 @@ text_on_checkbtn = ttk.Checkbutton(base_frame, text= "Keyboard mode", onvalue=1,
 text_on_checkbtn.place(x= 175, y= 12 )
 
 mouse_on_checkbtn = ttk.Checkbutton(base_frame, text= "Mouse mode", onvalue=1, offvalue= 0, variable=mouse_on)
-mouse_on_checkbtn.place(x= 290, y= 12)
+mouse_on_checkbtn.place(x= 285, y= 12)
+
+code_on_checkbtn = ttk.Checkbutton(base_frame, text= "Code-file mode", onvalue=1, offvalue= 0, variable=code_on)
+code_on_checkbtn.place(x= 380, y= 12 )
 ##
 
-base_frame.place(x= 5, y= 5, width=490, height= 80 )
+##
+code_path_change_btn = tk.Button(base_frame, text= "Change path", command= select_file)
+code_path_change_btn.place(x= 12, y= 75, width= 90)
+
+code_path_label = tk.Label(base_frame, text= "Path: " + data["code_path"] )
+code_path_label.place(x= 110, y= 78,)
+##
+
+base_frame.place(x= 5, y= 5, width=490, height= 110 )
 #
 
 
 
-#
+# Text Frame for writing text
 text_frame = tk.Frame(borderwidth=1, relief="solid")
 
 ##
@@ -160,10 +258,10 @@ text_label = tk.Label(text_frame, text = "Saved text: " + data["text"])
 text_label.place(x= 110, y= 35 )
 ##
 
-text_frame.place(x= 5, y= 90, width= 490, height= 65)
+text_frame.place(x= 5, y= 120, width= 490, height= 65)
 #
 
-#
+#Text Frame for wirting text
 mouse_frame = tk.Frame(borderwidth=1, relief="solid")
 
 ##
@@ -178,7 +276,7 @@ double_click_checkbtn.place(x= 5, y= 65)
 
 ##
 
-mouse_frame.place(x= 5, y= 160, width= 490, height= 100)
+mouse_frame.place(x= 5, y= 190, width= 490, height= 100)
 #
 
 
